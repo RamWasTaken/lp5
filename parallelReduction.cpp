4d1a402213ca944f3401d5cea97d18a572d2cf54
Ram Kardile <114078654+RamWasTaken@users.noreply.github.com>
2025-05-01 12:19:16 +0530

    Update parallelReduction.cpp
    minor changes

--- parallelReduction.cpp
+++ parallelReduction.cpp
@@ -1,67 +1,69 @@
 #include <iostream>
-// #include <vector>
 #include <omp.h>
 #include <climits>
 using namespace std;
-void min_reduction(int arr[], int n)
-{
+
+void min_reduction(int arr[], int n) {
     int min_value = INT_MAX;
-#pragma omp parallel for reduction(min : min_value)
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < min_value)
-        {
+    #pragma omp parallel for reduction(min: min_value)
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < min_value) {
             min_value = arr[i];
         }
     }
     cout << "Minimum value: " << min_value << endl;
 }
-void max_reduction(int arr[], int n)
-{
+
+void max_reduction(int arr[], int n) {
     int max_value = INT_MIN;
-#pragma omp parallel for reduction(max : max_value)
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > max_value)
-        {
+    #pragma omp parallel for reduction(max: max_value)
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max_value) {
             max_value = arr[i];
         }
     }
     cout << "Maximum value: " << max_value << endl;
 }
-void sum_reduction(int arr[], int n)
-{
-    int sum = 0;
-#pragma omp parallel for reduction(+ : sum)
-    for (int i = 0; i < n; i++)
-    {
+
+void sum_reduction(int arr[], int n) {
+    long long sum = 0;  // Prevent overflow
+    #pragma omp parallel for reduction(+: sum)
+    for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
     cout << "Sum: " << sum << endl;
 }
-void average_reduction(int arr[], int n)
-{
-    int sum = 0;
-#pragma omp parallel for reduction(+ : sum)
-    for (int i = 0; i < n; i++)
-    {
+
+void average_reduction(int arr[], int n) {
+    long long sum = 0;
+    #pragma omp parallel for reduction(+: sum)
+    for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
-    cout << "Average: " << sum / (double)(n) << endl;
+    cout << "Average: " << (double)sum / n << endl;  // Improved precision
 }
-int main()
-{
-    int *arr, n;
-    cout << "\n enter total no of elements=>";
+
+int main() {
+    int n;
+    cout << "Enter total number of elements: ";
     cin >> n;
-    arr = new int[n];
-    cout << "\n enter elements=>";
-    for (int i = 0; i < n; i++)
-    {
+
+    if (n <= 0) {
+        cerr << "Error: Array size must be positive." << endl;
+        return 1;
+    }
+
+    int* arr = new int[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+
     min_reduction(arr, n);
     max_reduction(arr, n);
     sum_reduction(arr, n);
     average_reduction(arr, n);
+
+    delete[] arr;  // Free memory
+    return 0;
 }
